6a8d31b7b8dbdeabaed3b7b25471e6ce3ed93f53
PeiLin <peilinlala@gmail.com>
2024-06-09 02:51:36 +0800

    Add Day 1 file

---
+++ Day 1/LESSON 3 DAY 1 - INPUT FUNCTION.py
@@ -0,0 +1,11 @@
+# Input function
+num1 = int(input())
+num2 = int(input())
+
+print(num1*num2)
+
+
+#Print lenght of word
+numOfLetters = len("Angela")
+print(numOfLetters)
+print(len(input()))
---
+++ Day 1/day-1-printing-start.py
@@ -0,0 +1,3 @@
+print("Hello World")
+print("Hello World123\nHello World123")
+print("Hello"+" PeiLin")
